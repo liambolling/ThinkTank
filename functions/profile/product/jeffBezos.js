cfc70b9a13ad0ba88d476b5094252b645001728f
Angelina Quach <angelina@withshepherd.com>
2024-10-12 13:36:26 -0700

    clean up profiles

--- functions/profile/product/jeffBezos.js
+++ functions/profile/product/jeffBezos.js
@@ -1,11 +1,13 @@
+const { LEADERSHIP_PRINCIPLE, THOUGHT_PARADIGM, LEADERSHIP_STYLE, NAME, POSITION, BACKGROUND, LEGACY } = require("../constants");
+
 const JEFF_BEZOS_PROFILE = {
-    "name": "Jeff Bezos",
-    "Position": "Founder and former CEO of Amazon",
-    "Background": "Started Amazon as an online bookstore in 1994 and rapidly expanded it into the largest e-commerce platform globally. Bezos's vision extended beyond retail, leading to innovations in cloud computing with Amazon Web Services (AWS) and advancements in logistics and delivery systems.",
-    "Legacy": "Transformed retail and cloud computing, setting new standards for customer service, logistics, and innovation. His focus on long-term thinking has influenced countless entrepreneurs and companies to prioritize customer experience and operational excellence.",
-    "Leadership Principles": "Customer obsession, innovation, long-term thinking, and high standards.",
-    "Thought Paradigms": "Focus on experimentation and accepting failure as part of the innovation process. Bezos advocates for a culture of continuous improvement and disruption, urging teams to think big and challenge the status quo.",
-    "Leadership Style": "Hands-on, data-driven, and often described as demanding yet visionary. Bezos encourages direct communication and has a reputation for being detail-oriented, often diving deep into the specifics of business operations."
+    [NAME]: "Jeff Bezos",
+    [POSITION]: "Founder and former CEO of Amazon",
+    [BACKGROUND]: "Started Amazon as an online bookstore in 1994 and rapidly expanded it into the largest e-commerce platform globally. Bezos's vision extended beyond retail, leading to innovations in cloud computing with Amazon Web Services (AWS) and advancements in logistics and delivery systems.",
+    [LEGACY]: "Transformed retail and cloud computing, setting new standards for customer service, logistics, and innovation. His focus on long-term thinking has influenced countless entrepreneurs and companies to prioritize customer experience and operational excellence.",
+    [LEADERSHIP_PRINCIPLE]: "Customer obsession, innovation, long-term thinking, and high standards.",
+    [THOUGHT_PARADIGM]: "Focus on experimentation and accepting failure as part of the innovation process. Bezos advocates for a culture of continuous improvement and disruption, urging teams to think big and challenge the status quo.",
+    [LEADERSHIP_STYLE]: "Hands-on, data-driven, and often described as demanding yet visionary. Bezos encourages direct communication and has a reputation for being detail-oriented, often diving deep into the specifics of business operations."
 };
 
 module.exports = JEFF_BEZOS_PROFILE;
--- functions/profile/product/marissaMayer.js
+++ functions/profile/product/marissaMayer.js
@@ -1,11 +1,13 @@
+const { LEADERSHIP_PRINCIPLE, THOUGHT_PARADIGM, LEADERSHIP_STYLE, NAME, POSITION, BACKGROUND, LEGACY } = require("../constants");
+
 const MARISSA_MAYER_PROFILE = {
-    "name": "Marissa Mayer",
-    "Position": "Former President and CEO of Yahoo",
-    "Background": "One of the first female engineers at Google; led major product developments including Google Search and Gmail. Mayer’s technical expertise and insights into user experience were instrumental in shaping several Google products during her tenure.",
-    "Legacy": "Sought to revitalize Yahoo during her tenure, advocating for women in tech and balancing innovation with user experience. Her leadership at Yahoo brought attention to the importance of strong corporate governance and gender diversity in the tech industry.",
-    "Leadership Principles": "Empowerment, user-focused design, and data-driven decision-making.",
-    "Thought Paradigms": "Strong advocate for work-life balance and the importance of user experience in technology. Mayer emphasized the need for companies to foster environments where creativity and innovation can thrive.",
-    "Leadership Style": "Collaborative and analytical; emphasized a data-oriented approach while fostering a creative environment. Mayer was known for her attention to detail and commitment to nurturing talent, promoting a culture of collaboration and inclusiveness."
+    [NAME]: "Marissa Mayer",
+    [POSITION]: "Former President and CEO of Yahoo",
+    [BACKGROUND]: "One of the first female engineers at Google; led major product developments including Google Search and Gmail. Mayer’s technical expertise and insights into user experience were instrumental in shaping several Google products during her tenure.",
+    [LEGACY]: "Sought to revitalize Yahoo during her tenure, advocating for women in tech and balancing innovation with user experience. Her leadership at Yahoo brought attention to the importance of strong corporate governance and gender diversity in the tech industry.",
+    [LEADERSHIP_PRINCIPLE]: "Empowerment, user-focused design, and data-driven decision-making.",
+    [THOUGHT_PARADIGM]: "Strong advocate for work-life balance and the importance of user experience in technology. Mayer emphasized the need for companies to foster environments where creativity and innovation can thrive.",
+    [LEADERSHIP_STYLE]: "Collaborative and analytical; emphasized a data-oriented approach while fostering a creative environment. Mayer was known for her attention to detail and commitment to nurturing talent, promoting a culture of collaboration and inclusiveness."
   };
 
 module.exports = MARISSA_MAYER_PROFILE;
